b800d2a7128ce93f66bd0f97313e71a39ff4d1a0
sayantan_codes <54500838+Sayantan-codes@users.noreply.github.com>
2020-12-17 13:14:28 +0530

    Update debug_exam.py

--- debug_exam.py
+++ debug_exam.py
@@ -49,9 +49,9 @@ def uniqueUpdate(data1, data2):
                 # to dictionary                
                 dupKeys[k] = [v1, v2]
                 del data1[k]
-            else:
-                # Add (k, v2) to data1
-                data1[k] = v2
+        else:
+             # Add (k, v2) to data1
+             data1[k] = v2
     # After processing all (k, v2) in
     # data2, return the dictionary
     return dupKeys
